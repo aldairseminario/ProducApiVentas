a0cd12ea98ff84c99ff709a1742bbfe9c636552e
aldairseminario <aldairseminario@gmail.com>
2024-09-27 17:13:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask , jsonify, send_file, Response
-from base import DescargarDatos , JsonVentas,JsonVentasxMes , Jsonproductosxdia , JsonTotal
+from flask import Flask , jsonify, send_file, Response , request
+from functions import CargarFiltros , NewConsultas
 from flask_cors import CORS
-import pandas as pd
 
 
 
@@ -9,37 +8,23 @@ app = Flask(__name__)
 
 CORS(app)
 
-@app.route('/datos')
-def EnviarDatos():
-    #return send_file("resultados.csv", mimetype='text/csv')
-    df = pd.read_csv('resultados.csv')
-    text_output = df.to_string(index=False)
-    return Response(text_output, mimetype='text/plain')
+@app.route('/f',methods=['GET'])
+def filtro():
+    data = request.args
+    d = data.get('d')
+    c,z,t = CargarFiltros(d)
+    return jsonify({"z":z,"c":c,"t":t})
 
-@app.route('/json')
-def DatosJson():
-    ventas = JsonVentas()
-    return jsonify(ventas)
+@app.route('/NewProc', methods=['POST'])
+def Newfiltro():
+    data = request.get_json()
+    newdata = NewConsultas(data)
+    return jsonify(newdata)
 
-@app.route('/ventasxmes')
-def ventasxmes():
-    ventas = JsonVentasxMes()
-    return jsonify(ventas)
-
-@app.route('/productosxdia')
-def productosxdia():
-    ventas = Jsonproductosxdia()
-    return jsonify(ventas)
-
-@app.route('/total')
-def total():
-    ventas = JsonTotal()
-    return jsonify(ventas)
-    
 
 @app.route('/')
 def index():
     return "Que haces aqui :/"
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=5000, debug=True)
